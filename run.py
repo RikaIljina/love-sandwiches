4ad6388e0273e277534f34116711af44b544f1c3
Vasilika Schnitzer <wasilika@hotmail.com>
2024-01-06 17:47:13 +0100

    Calculate surplus data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -52,6 +53,20 @@ def validate_data(values):
     
     return True
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock, calculate surplus for each item type
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    # pprint(stock)
+    stock_row = [int(num) for num in stock[-1]]
+    print(f'Stock row: {stock_row}')
+    print(f'Sales row: {sales_row}')
+    surplus = [x-y for x,y in (zip(stock_row, sales_row))]
+    return surplus
+    
+
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -62,7 +77,17 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
     
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    
+print("Welcome to Love Sandwiches Data Automation!")
+main()
+    
