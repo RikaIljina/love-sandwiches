cb995fb54b17213d7e19af196b1d0ce248b248ed
Vasilika Schnitzer <wasilika@hotmail.com>
2024-01-06 16:34:50 +0100

    Expand validation function to check for non-number entries

--- run.py
+++ run.py
@@ -21,14 +21,17 @@ def get_sales_data():
     """
     Get sales figures from user
     """
-    print("Please enter sales data from the last market:")
-    print("Data should be six numbers separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
-    
-    data_str = input("Enter data here:")
-    data_list = data_str.split(",")
-    validate_data(data_list)
-    
+    while True:
+            
+        print("Please enter sales data from the last market:")
+        print("Data should be six numbers separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+        
+        data_str = input("Enter data here:")
+        data_list = data_str.split(",")
+        if validate_data(data_list):
+            break
+        
     # print(f'Data provided is {data_list}')
     
 def validate_data(values):
@@ -38,9 +41,14 @@ def validate_data(values):
     or if there isn't exactly 6 values.
     """
     try:
-        if len(values) != 0:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(f'Exactly 6 values required, you provided {len(values)}')
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
-
+        return False
+    
+    return True
+        
+    
 get_sales_data()
