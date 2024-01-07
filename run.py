32862be4f86e40de1b73f7aeba4448c0d68e7b71
Vasilika Schnitzer <wasilika@hotmail.com>
2024-01-07 10:54:54 +0100

    ADd requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -28,14 +27,13 @@ def get_sales_data():
         print("Data should be six numbers separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
         
-        data_str = input("Enter data here:")
+        data_str = input("Enter data here:\n")
         sales_data = data_str.split(",")
         if validate_data(sales_data):
             break
         
     return sales_data
         
-    # print(f'Data provided is {data_list}')
     
 def validate_data(values):
     """
